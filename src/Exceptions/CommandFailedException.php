aa9478ca5975ec2d71e8c49c5c8c2aca683db7f1
Jan Wytze <jwzuid@live.nl>
2017-03-06 12:07:44 +0100

    Added httpCode to exception

--- src/Exceptions/CommandFailedException.php
+++ src/Exceptions/CommandFailedException.php
@@ -4,6 +4,29 @@ namespace Jwz104\BitcoinAccounts\Exceptions;
 
 class CommandFailedException extends \Exception {
 
+    /**
+     * The http error code
+     *
+     * @var int
+     */
+    protected $httpCode;
+
+    /**
+     * Instantiate a new CommandFailedException instance.
+     *
+     * @param $errorCode int The http error code
+     * @return void
+     */
+    public function __construct($errorCode)
+    {
+        $this->httpCode = $errorCode;
+    }
+
+    /**
+     * The message that gets returned when getMessage() is called
+     *
+     * @return void
+     */
     public function __toString()
     {
         __toString('Could not execute the command'); 
