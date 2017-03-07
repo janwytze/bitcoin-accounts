99de4c3a1b7eecfaf35f75e5f22eece43da6987e
Jan Wytze <jwzuid@live.nl>
2017-03-07 12:00:09 +0100

    Added emptyAccountToAddress, setAddressUser, sendToUser and sendToAddress

--- src/Models/BitcoinUser.php
+++ src/Models/BitcoinUser.php
@@ -81,4 +81,54 @@ class BitcoinUser extends Model
     {
         return BitcoinAccounts::createAddress($this);
     }
+
+    /**
+     * Send bitcoins to an address and return the txid
+     * the amount of fee defined in the config file will be added to the amount
+     *
+     * @param $address Jwz104\BitcoinAccounts\Models\BitcoinAddress To address
+     * @param $amount double The amount of bitcoins
+     * @param $fee double The amount of fee, leave empty for default amount
+     * @return string
+     */
+    public function sendToAddress($address, $amount, $fee = null)
+    {
+        return BitcoinAccounts::sendToAddress($this, $address, $amount, $fee);
+    }
+
+    /**
+     * Send bitcoins to an user
+     *
+     * @param $touser Jwz104\BitcoinAccounts\Models\BitcoinUser The destination user
+     * @param $amount double The amount of bitcoins
+     * @return bool
+     */
+    public function sendToUser(BitcoinUser $touser, $amount)
+    {
+        return BitcoinAccounts::sendToUser($this, $touser, $amount);
+    }
+
+    /**
+     * Change the user on the address to this user
+     *
+     * @param $address Jwz104\BitcoinAccounts\Models\BitcoinAddress The bitcoin address
+     * @return bool
+     */
+    public function setAddressUser(BitcoinAddress $address)
+    {
+        return BitcoinAccounts::setAddressUser($this, $address);
+    }
+
+    /**
+     * Transfer all bitcoin of the user to an address and return the txid
+     *
+     * @param $user Jwz104\BitcoinAccounts\Models\BitcoinUser The user
+     * @param $address string The address
+     * @param $fee double The fee, null for default fee
+     * @return string
+     */
+    public function emptyAccountToAddress($address, $fee = null)
+    {
+        return BitcoinAccounts::emptyAccountToAddress($this, $address, $fee);
+    }
 }
