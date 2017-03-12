467c377720e4cca2174419c8dcef7bd9aa680c13
Jan Wytze <jwzuid@live.nl>
2017-03-12 17:48:30 +0100

    Added validateAddress

--- src/BitcoinAccounts.php
+++ src/BitcoinAccounts.php
@@ -324,6 +324,43 @@ class BitcoinAccounts {
     }
 
     /**
+     * Check if the given address is valid
+     *
+     * @param $address string The bitcoin address to check
+     * @return boolean
+     */
+    public function validateAddress($address)
+    {
+        $alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+        $out = array_fill(0, 25, 0);
+        for ($i=0;$i<strlen($address);$i++) {
+            if (($p=strpos($alphabet, $address[$i]))===false) {
+                return false;
+            }
+            $c = $p;
+            for ($j = 25; $j--;) {
+                $c += (int)(58 * $out[$j]);
+                $out[$j] = (int)($c % 256);
+                $c /= 256;
+                $c = (int)$c;
+            }
+            if ($c != 0) {
+                return false;
+            }
+        }
+        $decoded = '';
+        foreach ($out as $val) {
+            $decoded .= chr($val);
+        }
+        $d1 = hash('sha256', substr($decoded,0,21), true);
+        $d2 = hash('sha256', $d1, true);
+        if(substr_compare($decoded, $d2, 21, 4)){
+            return false;
+        }
+        return true;
+    }
+
+    /**
      * List all the unspent bitcoins
      *
      * @param $listlocked boolean List the locked transactions
